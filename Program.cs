5a8cf2e9aa60a3c2cecb896d97e034bf9b371be3
Marcio de Andrade <gmandrade1@hotmail.com>
2023-12-13 19:14:52 -0400

    registro interface e repositório

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
+using WebApiAspNetCoreDapper.Context;
+using WebApiAspNetCoreDapper.Contracts;
+using WebApiAspNetCoreDapper.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSingleton<DapperContext>();
+builder.Services.AddScoped<IEmpresaRepository, EmpresaRepository>();
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
