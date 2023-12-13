f42fdc0112baf65562863bfd35a6fe0ba9999600
Marcio de Andrade <gmandrade1@hotmail.com>
2023-12-13 07:38:10 -0400

    Criação inicial do Repositorio obedecendo o Contrato e exclusão de classes do template

---
+++ Repository/EmpresaRepository.cs
@@ -0,0 +1,54 @@
+﻿using WebApiAspNetCoreDapper.Contracts;
+using WebApiAspNetCoreDapper.Dto;
+using WebApiAspNetCoreDapper.Entities;
+
+namespace WebApiAspNetCoreDapper.Repository
+{
+    public class EmpresaRepository : IEmpresaRepository
+    {
+        public Task<Empresa> CreateEmpresa(EmpresaForCreationDto empresa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateMultipleEmpresas(List<EmpresaForCreationDto> empresas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEmpresa(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Empresa> GetEmpresa(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Empresa> GetEmpresaByFuncionarioId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Empresa> GetEmpresaFuncionariosMultipleResults(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Empresa>> GetEmpresas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Empresa>> GetEmpresasFuncionariosMultipleMapping()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEmpresa(int id, EmpresaForUpdateDto empresa)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
